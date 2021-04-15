908cc97bd2471a3260a733dffd5c8c05ddd1f555
LeanBlauth <le4ndroblauth@gmail.com>
2021-04-14 23:37:38 -0300

    Chess game project created

---
+++ S_016_ChessGame/README.md
@@ -0,0 +1,3 @@
+## Motif
+
+This is a major project that will exercise the greater part of all that was introduced previously, in a bigger and more complex problem
---
+++ S_016_ChessGame/src/applications/ChessGame.java
@@ -0,0 +1,23 @@
+package applications;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ChessGame {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		scan.close();
+	}
+
+}

5d98e9c7dd61f51a88e58bb77f8f02ba7b3fe99f
LeanBlauth <le4ndroblauth@gmail.com>
2021-04-18 17:30:00 -0300

    Implemented possible moves for the King

--- S_016_ChessGame/src/chess/pieces/King.java
+++ S_016_ChessGame/src/chess/pieces/King.java
@@ -3,6 +3,7 @@ package chess.pieces;
 import chess.ChessPiece;
 import chess.Color;
 import gameboard.Board;
+import gameboard.Position;
 
 public class King extends ChessPiece {
 
@@ -17,8 +18,40 @@ public class King extends ChessPiece {
 
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean[][] moves = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		return moves;
-	}
+		boolean[][] allowedMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		// Check above
+		allowedMoves = stepValidation(allowedMoves, -1, +0);
+		
+		// Check below
+		allowedMoves = stepValidation(allowedMoves, +1, +0);
 
+		// Check to the left
+		allowedMoves = stepValidation(allowedMoves, 0, -1);
+		
+		// Check to the right
+		allowedMoves = stepValidation(allowedMoves, +0, +1);
+		
+		// Check right & above
+		allowedMoves = stepValidation(allowedMoves, -1, +1);
+		
+		// Check right & below
+		allowedMoves = stepValidation(allowedMoves, +1, +1);
+		
+		// Check left & above
+		allowedMoves = stepValidation(allowedMoves, -1, -1);		
+		
+		// Check left & below
+		allowedMoves = stepValidation(allowedMoves, +1, -1);
+		
+		return allowedMoves;
+	}
+	
+	public boolean[][] stepValidation(boolean[][] mat, int rowStep, int columnStep) {
+		Position assessedPosition = new Position(position.getRow() + rowStep, position.getColumn() + columnStep);
+		if (getBoard().positionExists(assessedPosition) && (!getBoard().isPositionOccupied(assessedPosition)||isThereOpponentPiece(assessedPosition))) {		
+			mat[assessedPosition.getRow()][assessedPosition.getColumn()] = true;
+		}
+		return mat;
+	}
 }

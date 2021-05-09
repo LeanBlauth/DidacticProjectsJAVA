0c3241a943f15fcaed05e792651e82e925e8d21e
LeanBlauth <le4ndroblauth@gmail.com>
2021-05-09 10:13:51 -0300

    DAOFactory and SellerDAO implementation

--- S_021_DAO-JDBC/src/application/Program.java
+++ S_021_DAO-JDBC/src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,10 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(7, "ronald mcdolans", "keke@kuuk.com", new Date(), 3000.0, obj);
+		
+		
+		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
+		
 		
 		System.out.println(seller);
 	}
---
+++ S_021_DAO-JDBC/src/model/dao/DAOFactory.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.dao.impl.SellerDAO_JDBC;
+
+public class DAOFactory {
+	
+	public static SellerDAO createSellerDAO() {
+		
+		return new SellerDAO_JDBC();
+		
+	}
+
+}

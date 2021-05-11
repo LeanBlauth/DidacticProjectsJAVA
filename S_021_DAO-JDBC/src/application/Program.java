a3820ca00bd96a7335faeee2385f6bea43dbdad5
LeanBlauth <le4ndroblauth@gmail.com>
2021-05-10 22:32:00 -0300

    SellerDAO_JDBC method: update

--- S_021_DAO-JDBC/src/application/Program.java
+++ S_021_DAO-JDBC/src/application/Program.java
@@ -11,28 +11,34 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
-		
+
 		System.out.println("==== TEST 1 - seller method: findById ============");
 		Seller seller1 = sellerDAO.findById(3);
 		System.out.println(seller1);
-		
+
 		System.out.println("\n==== TEST 2 - seller method: findByDepartment ====");
-		List<Seller> sellerList = sellerDAO.findByDepartment(new Department(1,null));
+		List<Seller> sellerList = sellerDAO.findByDepartment(new Department(1, null));
 		for (Seller sel : sellerList) {
 			System.out.println(sel);
 		}
-		
+
 		System.out.println("\n==== TEST 3 - seller method: findAll =============");
 		sellerList = sellerDAO.findAll();
 		for (Seller sel : sellerList) {
 			System.out.println(sel);
 		}
-		
+
 		System.out.println("\n==== TEST 4 - seller method: insert ==============");
-		Seller seller2 = new Seller(null, "Lorald McDolans", "lorald@mcdolans.com", new Date(), 3000.0, new Department(2, null));
+		Seller seller2 = new Seller(null, "Lorald McDolans", "lorald@mcdolans.com", new Date(), 3000.0,
+				new Department(2, null));
 		sellerDAO.insert(seller2);
 		System.out.println("Seller inserted ! New ID = " + seller2.getId());
+
+		System.out.println("\n==== TEST 5 - seller method: update ==============");
+		sellerDAO.update(new Seller(9, "Rudolph", "rudy@gmail.com", new Date(), 3500.0,
+				new Department(3, null)));
+		System.out.println("Update completed");
 	}
 }
--- S_021_DAO-JDBC/src/model/dao/impl/SellerDAO_JDBC.java
+++ S_021_DAO-JDBC/src/model/dao/impl/SellerDAO_JDBC.java
@@ -1,6 +1,7 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -64,8 +65,28 @@ public class SellerDAO_JDBC implements SellerDAO {
 
 	@Override
 	public void update(Seller seller) {
-		// TODO Auto-generated method stub
-
+		
+		PreparedStatement st = null;
+		
+		try {
+		st = conn.prepareStatement(
+				"UPDATE seller "
+				+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+				+ "WHERE Id = ? ",
+				Statement.RETURN_GENERATED_KEYS);
+		st.setString(1, seller.getName());
+		st.setString(2, seller.getEmail());
+		st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+		st.setDouble(4, seller.getBaseSalary());
+		st.setInt(5, seller.getDepartment().getId());
+		st.setInt(6, seller.getId());
+		
+		st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
